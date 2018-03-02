859cbfaa4cb14601ac4ac440dbccceee747e9ba2
Hugo Leite <hugo.leite@zottomobile.com.br>
2018-03-02 11:22:47 -0300

    log

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook', (req, res) => {
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id
             console.log('Sender PSID: ' + sender_psid)
-            console.log(webhook_event)
+            console.log(webhook_event.message)
 
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
