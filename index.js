3b6ed5f478e2ef9dd9c92c7c8cd5d5c856f6ec5a
Hugo Leite <hugo.leite@zottomobile.com.br>
2018-03-02 11:09:19 -0300

    Faltou passar callSendAPI por parametro

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ app.post('/webhook', (req, res) => {
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message)
+                handleMessage(sender_psid, webhook_event.message, callSendAPI)
             } else if (webhook_event.postback) {
-                handlePostback(sender_psid, webhook_event.postback)
+                handlePostback(sender_psid, webhook_event.postback, callSendAPI)
             }
         })
 
